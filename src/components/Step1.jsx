6ef4dc39817a4ae727b0c156c670afb51b951439
Andrey Oreshin <avoreshin@gmail.com>
2022-06-07 20:46:04 +0300

    feat: add Step1

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -5,32 +5,49 @@ import { Form } from "./Form";
 import { Input } from "./Input";
 import { useForm } from "react-hook-form";
 import { PrimaryButton } from "./PrimaryButton";
+// import yup from "yup";
+
+// const schema = yup.object().shape({
+//   firstName: yup
+//     .string()
+//     .matches(/^[^0-9]*)$/, "First name should not contain number")
+//     .required("First name is a required field")
+// });
 
 export const Step1 = () => {
-  const { register, handleSubmit, formState:{errors} } = useForm({
-    mode: 'onBlur',
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
   });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <>
       <MainContainer>
         <Typography>🦄 Step1</Typography>
-        <Form>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Input
             {...register("firstName")}
             id="firstName"
             type="text"
             label="First Name"
-            
           />
           <Input
             {...register("lastName")}
             id="lastName"
             type="text"
             label="Last Name"
-            
           />
+
+          <PrimaryButton>Next</PrimaryButton>
         </Form>
-        <PrimaryButton>Next</PrimaryButton>
+
+        <p>{onSubmit}</p>
       </MainContainer>
     </>
   );
