d808f119707052cf64fcadfe0f8f136683debf9b
Andrey Oreshin <avoreshin@gmail.com>
2022-06-11 21:09:33 +0300

    feat: add Step3

---
+++ src/components/Step3.jsx
@@ -0,0 +1,20 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { FileInput } from './FileInput'
+import { Form } from './Form'
+import { MainContainer } from './MainContainer'
+
+export const Step3 = () => {
+
+const {control} = useForm;
+
+  return (
+    <MainContainer>
+        <Typography>🦄 Step3</Typography>
+        <Form>
+            <FileInput name="files" control={control}/>
+        </Form>
+    </MainContainer>
+  )
+}
